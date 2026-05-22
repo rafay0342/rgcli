21d242435d64e44acf879f6f4c7bd7172f18d9ff
root <root@RafayFlash.localdomain>
2026-05-22 11:01:12 +0500

    Configure Homebrew for NPM package registry and update .npmignore

--- distribution/rgcli.rb
+++ distribution/rgcli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Rgcli < Formula
   desc "RafayGen - The Ultimate Agentic Coding CLI"
   homepage "https://github.com/RafayGen/rgcli"
-  url "https://github.com/rafay0342/rgcli/archive/refs/tags/v1.3.3.tar.gz"
-  sha256 "4a15375ba514975bfec597f6401689909eafe29ba4bb6a25e8145d1c547d2847"
+  url "https://registry.npmjs.org/rafaygen-cli/-/rafaygen-cli-1.3.3.tgz"
+  sha256 "6a7f2ff1bd2ae21ef82422427b39b3bc0d40a95b8274aa716f5e56adead221f6"
   license "ISC"
 
   depends_on "node"
